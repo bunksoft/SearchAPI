3a659bb69ff75441c2cefae7e515f00f50ae8861
Amrit Kshetri <it.dumb@gmail.com>
2013-01-03 21:25:32 +0545

    livesearch testing phase edit

--- main.py
+++ main.py
@@ -6,25 +6,25 @@ import get_livesearch
 import get_search_query
 
 
-get_logpoints.get()
+#get_logpoints.get()
 #print '------------------'
 #print '\n\n'
 #print '------------------'
-get_repos.get()
+#get_repos.get()
 #print '------------------'
 #print '\n\n'
 #print '------------------'
-get_devices.get()
+#get_devices.get()
 #print '------------------'
 #print '\n\n'
 #print '------------------'
-get_timezone.get()
+#get_timezone.get()
 #print '------------------'
 #print '\n\n'
 #print '------------------'
-response = get_search_query.get()
+#response = get_search_query.get()
 
 #print '------------------'
 #print '\n\n'
 #print '------------------'
-#get_livesearch.get()
+get_livesearch.get()
