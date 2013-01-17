3465897e8f3d64b9d72863c1b0a15d3e7fcc6b93
ln2khanal <ln2khanal@gmail.com>
2013-01-17 14:09:54 +0545

    credentials changed

--- Config.py
+++ Config.py
@@ -7,22 +7,36 @@ __date__ ="$Dec 26, 2012 3:25:45 PM$"
 class Config:
     def __init__(self):
         self.request_type = 'https'
-        self.ip = '192.168.2.205'
+        self.ip = 'localhost'
 #        self.port = 5000
 #        self.username = 'mytest'
 #        self.secret_key = 'a2b29749ff3fcff8c3cb46315627f8b0'
 #        
 #        LI user for 205
-        self.username = 'ln2khanal'
-        self.secret_key = '36ad226f539a446f2ce8a98c883d34c5'
+#        self.username = 'ln2khanal'
+#        self.secret_key = '36ad226f539a446f2ce8a98c883d34c5'
 #        
 #        self.username = 'bunkdeath'
 #        self.secret_key = 'bcb070c22969225e0f517b403391bcde'
 
 #        User for my local LI-Lekhnath Khanal
-#        self.username = 'ln2khanal'
-#        self.secret_key = '3b4d01f95fa14fb55cd15a26b87c7c1c'
+        self.username = 'admin'
+        self.secret_key = '60af8fc63fd51d126b920bdc3180dddf'
+
+
 #        self.username = 'admin'
 #        self.secret_key = '29cc708f5cee084bb9d7b8c704d6f8e3'
-#210
+
+#        210
 #        self.secret_key = '1b1bd10635a58b889a5e721c59d238e5'
+
+
+
+
+#192.168.2.10
+#        self.username = 'admin'
+#        self.secret_key = 'c5856f825a88e26e1d04456df01f6562'
+        
+#        self.username = 'admin'
+#        self.secret_key = 'ec638e664f867a855badefaa05e04c4a'
+
